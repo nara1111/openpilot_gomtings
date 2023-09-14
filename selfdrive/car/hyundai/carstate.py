117c80ed20470e95d3dc604f9d45fd861991f08c
leesangwoo <tkddn4508@gmail.com>
2023-09-14 23:31:00 +0900

    remuve print

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -246,7 +246,6 @@ class CarState(CarStateBase):
     if self.CP.openpilotLongitudinalControl and CruiseStateManager.instance().cruise_state_control:
       available = ret.cruiseState.available if self.CP.sccBus == 2 else -1
       CruiseStateManager.instance().update(ret, self.main_buttons, self.cruise_buttons, BUTTONS_DICT, available)
-      print("self.CP.openpilotLongitudinalControl")
 
     return ret
 
